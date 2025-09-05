65cce2c2ff8b66a58d2bf9b6b5f3a4d6c7518861
soumyajit11 <prantobanik30@gmail.com>
2025-09-05 11:19:06 +0530

    deployment configuration for AWS

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -7,13 +7,16 @@ class PredictPipeline:
     def __init__(self):
         pass
     def predict(self,features):
-        model_path='artifacts\model.pkl'
-        preprocessor_path='artifacts\preprocessor.pkl'
-        model=load_object(file_path=model_path)
-        preprocessor=load_object(file_path=preprocessor_path)
-        data_scaled=preprocessor.transform(features)
-        preds=model.predict(data_scaled)
-        return preds
+        try:
+            model_path='artifacts\model.pkl'
+            preprocessor_path='artifacts\preprocessor.pkl'
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomException(e,sys)
 
 class CustomData:
     def __init__(self,
